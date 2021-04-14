3e2fbbfecc926790114f48467f57b1871f41bb18
kk <quique1904@gmail.com>
2021-04-14 18:41:53 +0200

    Entity + liquibase

--- reservas/src/main/java/com/alquiler/reservas/entity/Role.java
+++ reservas/src/main/java/com/alquiler/reservas/entity/Role.java
@@ -1,5 +1,6 @@
 package com.alquiler.reservas.entity;
 
+
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -11,17 +12,21 @@ import javax.persistence.Id;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-public class Role implements Serializable {
+public class Role implements Serializable{
 
-	private static final long serialVersionUID = -2969524610059270447L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6353963609310956029L;
+
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)	
 	private Long id;
-	
+
 	@Column
 	private String name;
-	
+
 	@Column
 	private String description;
 
@@ -49,8 +54,41 @@ public class Role implements Serializable {
 		this.description = description;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -58,5 +96,5 @@ public class Role implements Serializable {
 		return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
 	}
 
-	
+
 }
--- reservas/src/main/java/com/alquiler/reservas/entity/User.java
+++ reservas/src/main/java/com/alquiler/reservas/entity/User.java
@@ -9,47 +9,53 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Transient;
+
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.JoinColumn;
-
-@Entity
+@Entity 
 public class User implements Serializable{
 
-	private static final long serialVersionUID = -6833167247955613395L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1671417246199538663L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column 
+
+	@Column
 	private String firstName;
-	@Column 
+	@Column
 	private String lastName;
-	@Column(unique = true) 
+	@Column
 	private String email;
-	@Column(unique = true) 
+	@Column
 	private String username;
 	@Column
 	private String password;
-	
-	@Transient 
+
+	@Transient
 	private String confirmPassword;
-	
+
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name="user_roles"
-		,joinColumns=@JoinColumn(name="user_id")
-		,inverseJoinColumns=@JoinColumn(name="role_id"))
-	private Set roles;
-	
-	public User() {	}
-	
+	@JoinTable(name = "user_roles",
+			joinColumns=@JoinColumn(name="user_id"),
+			inverseJoinColumns=@JoinColumn(name="role_id"))
+	private Set<Role> roles;
+
+	public User() {
+		super();
+	}
+
 	public User(Long id) {
+		super();
 		this.id = id;
-	}       //Getters, Setters, HashCode, Equals & ToString Functions
+	}
 
 	public Long getId() {
 		return id;
@@ -107,16 +113,12 @@ public class User implements Serializable{
 		this.confirmPassword = confirmPassword;
 	}
 
-	public Set getRoles() {
+	public Set<Role> getRoles() {
 		return roles;
 	}
 
-	public void setRoles(Set roles) {
+	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
 	}
 
 	@Override
@@ -125,6 +127,72 @@ public class User implements Serializable{
 				+ ", username=" + username + ", password=" + password + ", confirmPassword=" + confirmPassword
 				+ ", roles=" + roles + "]";
 	}
-	
-	
+/*
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((confirmPassword == null) ? 0 : confirmPassword.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((roles == null) ? 0 : roles.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (confirmPassword == null) {
+			if (other.confirmPassword != null)
+				return false;
+		} else if (!confirmPassword.equals(other.confirmPassword))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (roles == null) {
+			if (other.roles != null)
+				return false;
+		} else if (!roles.equals(other.roles))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+*/
 }
