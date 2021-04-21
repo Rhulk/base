647ee0415d656bb98b672b7ee9710973ca1cb4d0
rhulk <quique1904@gmail.com>
2021-04-22 00:20:20 +0200

    up

--- reservas/src/main/java/com/alquiler/reservas/controller/DemoExceptionHandler.java
+++ reservas/src/main/java/com/alquiler/reservas/controller/DemoExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class DemoExceptionHandler {
 
+	//up
     @ExceptionHandler(Exception.class)
     public String exceptionHandler(){
         return "error";
