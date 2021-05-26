a747dc1b36f1518af444a32683531788d184cb6c
kk <quique1904@gmail.com>
2021-05-26 16:17:52 +0200

    Ajustes Search List

--- src/main/java/com/alquiler/reservas/service/UserServiceImpl.java
+++ src/main/java/com/alquiler/reservas/service/UserServiceImpl.java
@@ -79,12 +79,13 @@ public class UserServiceImpl implements UserService{
 	 *  Update User
 	 *  
 	 */
+	@Override
 	@PreAuthorize("isAuthenticated()")  
 	public User updateUser(User fromUser)  throws Exception {
 		User toUser = getUserById(fromUser.getId());
 		mapUser(fromUser, toUser);
 		return userRepository.save(toUser);
-	}
+	} 
 	
 	/*
 	 *  For Active user
