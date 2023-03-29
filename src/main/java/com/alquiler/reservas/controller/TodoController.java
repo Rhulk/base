5f5dbcfff30c579e37c05e6f9a07d90959c36d8a
rhulk <quique1904@gmail.com>
2023-03-29 18:12:38 +0200

    Recuperado todo desde el controler -falla el html

--- src/main/java/com/alquiler/reservas/controller/TodoController.java
+++ src/main/java/com/alquiler/reservas/controller/TodoController.java
@@ -1,15 +1,31 @@
 package com.alquiler.reservas.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.alquiler.reservas.entity.Estado;
+import com.alquiler.reservas.service.TodoService;
 
 @Controller
 public class TodoController {
 	
+	@Autowired
+	TodoService todoService;
+	
 	@GetMapping("/todolist")
 	public String listTodo() {
+		List<Estado> estados = new LinkedList<Estado>();
+		
+		estados.add(Estado.Inicial);
+		
+		System.out.println(estados.get(0).toString());
 		System.out.println("Listado de To-dos");
-		return "/todo/todo-list";
+		System.out.println(todoService.getByEstado(Estado.EnProceso));
+		return "todo-list";
 	}
 
 }
--- src/main/java/com/alquiler/reservas/service/TodoServiceImpl.java
+++ src/main/java/com/alquiler/reservas/service/TodoServiceImpl.java
@@ -3,11 +3,13 @@ package com.alquiler.reservas.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.alquiler.reservas.entity.Estado;
 import com.alquiler.reservas.entity.Todo;
 import com.alquiler.reservas.repository.TodoRepository;
 
+@Service
 public class TodoServiceImpl implements TodoService {
 	
 	@Autowired
