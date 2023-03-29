e2421852e893bd084004b990b63b6fbfae0bab4a
rhulk <quique1904@gmail.com>
2023-03-29 12:39:03 +0200

    implentando metodo find personalizado

--- src/main/java/com/alquiler/reservas/repository/TodoRepository.java
+++ src/main/java/com/alquiler/reservas/repository/TodoRepository.java
@@ -1,9 +1,16 @@
 package com.alquiler.reservas.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alquiler.reservas.entity.Estado;
 import com.alquiler.reservas.entity.Todo;
 
+@Repository
 public interface TodoRepository  extends CrudRepository<Todo, Long>{
 
+	
+	public List<Todo> findByEstado(Estado estado);
 }
