3f5230bb5901f3a7e4a5e85dd20f73a0cb6d3175
rhulk <quique1904@gmail.com>
2023-04-13 10:29:34 +0200

    Ajustes en el componente dataTable.js

--- src/main/resources/static/js/sql.js
+++ src/main/resources/static/js/sql.js
@@ -410,3 +410,11 @@ function getDereMas(valor){
 function getDereMenos(valor){
 
 }
+
+	    $(document).ready(function() {
+	        //Asegurate que el id que le diste a la tabla sea igual al texto despues del simbolo #
+	        console.log("DataTable Check- todoList");
+	        $('#userList').DataTable();
+	        $('#todoList').DataTable();
+	    } );
+	
